40aba31519bdeaac28b04c099ae6068cb307aa15
Vladim1r37 <vl4d1m1r37@gmail.com>
2018-11-23 20:07:27 +0400

    добавлен механизм отключения клиента от сервера по истечении 120 секунд, добавление собеседника в блэклист и удаление из него

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -7,6 +7,7 @@ import java.net.Socket;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.ArrayList;
 
 public class ClientHandler {
 
@@ -15,87 +16,120 @@ public class ClientHandler {
     private DataInputStream in;
     private Server server;
     private String nick;
+    private ArrayList<String> blackList;
+
 
     public ClientHandler(Server server, Socket socket) {
         try {
+            this.blackList = new ArrayList<>();
             this.socket = socket;
             this.server = server;
             this.in = new DataInputStream(socket.getInputStream());
             this.out = new DataOutputStream(socket.getOutputStream());
+            long startTime = System.currentTimeMillis();
 
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        //цикл авторизации
-                        while (true) {
-                            String str = in.readUTF();
+
+            new Thread(() -> {
+                try {
+                    //цикл авторизации
+                    while (true) {
+                        String str = in.readUTF();
+                        if (System.currentTimeMillis() - startTime < 120000) {
+
+                            if (str.startsWith("/signup")) {
+                                String[] tokens = str.split(" ");
+                                if (AuthService.signUp(tokens[1], tokens[2], tokens[3])) {
+                                    sendMsg("Регистрация прошла успешно");
+                                } else sendMsg("Недопустимый логин/никнэйм");
+                            }
+
                             if (str.startsWith("/auth")) {
                                 String[] tokens = str.split(" ");
                                 String newNick = AuthService.getNickname(tokens[1], tokens[2]);
                                 if (newNick != null) {
+                                    if (!server.isNickBusy(newNick)) {
                                     sendMsg("/authok");
                                     nick = newNick;
-                                    server.subscribe(ClientHandler.this);
-                                    server.addByNick(nick, ClientHandler.this);
+                                    server.subscribe(this);
+                                    server.addByNick(nick, this);
                                     break;
+                                    } else sendMsg("Учетная запись уже используется!");
                                 } else {
                                     sendMsg("Неверный логин/пароль");
                                 }
                             }
+
+                        } else {
+                            sendMsg("Превышено время ожидания авторизации");
+                            sendMsg("/autherror");
+                            return;
                         }
-                        //рабочий цикл
-                        while (true) {
-                            String str = in.readUTF();
-                            String time = LocalTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT));
+                    }
+                    //рабочий цикл
+                    while (true) {
+                        String str = in.readUTF();
+                        String time = LocalTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT));
+                        if (str.startsWith("/")) {
+
                             if (str.equals("/end")) {
                                 out.writeUTF("/serverclosed");
                                 break;
                             }
                             if (str.startsWith("/pm")) {
-                                String[] tokens = str.split(" ");
+                                String[] tokens = str.split(" ", 3);
                                 ClientHandler pmClient = server.getByNick(tokens[1]);
-                                if ( pmClient != null) {
-                                    String pm = "";
-                                    for (int i = 2; i < tokens.length; i++) {
-                                        pm += tokens[i] + " ";
-                                    }
-                                    pmClient.sendMsg(String.format("%s from %s: %s", time, nick, pm));
-                                    sendMsg(String.format("%s to %s: %s", time, tokens[1], pm));
+                                if (pmClient != null) {
+                                    pmClient.sendMsg(String.format("%s from %s: %s", time, nick, tokens[2]));
+                                    sendMsg(String.format("%s to %s: %s", time, tokens[1], tokens[2]));
                                 } else {
                                     sendMsg(String.format("%s is not connected", tokens[1]));
                                 }
-                            } else {
-                                server.broadcastMsg(String.format("%s %s: %s", time, nick, str));
                             }
+                            if (str.startsWith("/blacklist")) {
+                                String[] tokens = str.split(" ");
+                                blackList.add(tokens[1]);
+                                sendMsg("Вы добавили пользователя с ником " + tokens[1] + " в черный список!");
+                            }
+                            if (str.startsWith("/unblock")) {
+                                String[] tokens = str.split(" ");
+                                blackList.remove(tokens[1]);
+                                sendMsg("Вы убрали пользователя с ником " + tokens[1] + " из черного списка!");
+                            }
+                        } else {
+                            server.broadcastMsg(this, String.format("%s %s: %s", time, nick, str));
                         }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        in.close();
                     } catch (IOException e) {
                         e.printStackTrace();
-                    } finally {
-                        try {
-                            in.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        try {
-                            out.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        try {
-                            socket.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        server.unsubscribe(ClientHandler.this);
-                        server.removeByNick(nick);
                     }
+                    try {
+                        out.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    server.unsubscribe(this);
+                    server.removeByNick(nick);
                 }
             }).start();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    public String getNick() {
+        return nick;
+    }
+
 
     public void sendMsg(String msg) {
         try {
@@ -104,4 +138,9 @@ public class ClientHandler {
             e.printStackTrace();
         }
     }
+
+    public boolean checkBlackList(String nick) {
+        return blackList.contains(nick);
+    }
+
 }
